62815565fadcf580e9ee95a996d1d8ff97bbbb8b
JasperCheung <jcheung6@stuy.edu>
2018-02-14 23:05:06 -0500

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -1,34 +1,40 @@
 from pymongo import MongoClient
 
-c = MongoClient('lisa.stuy.edu')
+c = MongoClient('lisa.stuy.edu') #connection start
 
-db = c.test
-border = db.restaurants
+db = c.test #database object
+border = db.restaurants #Border Colliection
 
+#prints all resturants in a given borough
 def borough(boro):
-    result = border.find({'borough': boro})
+    result = border.find({'borough': boro}) 
     print("All restaurants in borough " + boro)
+    #find() returns a iterator, loop
     for item in result:
         print(item)
 
+#prints all resturants in zipcode 
 def zipcode(code):
     result = border.find({'address.zipcode': code})
     print("All restaurants in zipcode " + code)
     for item in result:
         print (item)
 
+#prints all resturants in a zipcode with a certain grade
 def zipgrade(code, grade):
-    result = border.find({'$and' : [{'address.zipcode': code}, {'grades.grade':grade}]})
+    result = border.find({'$and' : [{'address.zipcode': code}, {'grades.grade':grade}]}) #AND OPERATOR
     print("All restaurants in zipcode " + code + " with grade " + grade)
     for item in result:
         print (item)
 
+#prints all resturants in a zipcode within a certain score
 def zipbelow(code, score):
     result = border.find({'$and' : [{'address.zipcode': code}, {'grades.score': {'$lt':score}}]})
     print("All restaurants in zipcode " + code + " with score below " + str(score))
     for item in result:
         print (item)
-
+        
+#look at the paramaters 
 def clever(cuisine, boro):
     result = border.find({'$and' : [{'cuisine': cuisine}, {'borough': boro}]})
     print("All " + cuisine + " restaurants in borough " + boro)
